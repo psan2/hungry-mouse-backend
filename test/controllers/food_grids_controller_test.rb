e83d49bd98d8195dc5ab3e01f0bc2428565cdd84
Pat <patrick.s.santucci@gmail.com>
2019-06-17 13:55:03 +0100

    folders

---
+++ test/controllers/food_grids_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FoodGridsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/food_grid_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FoodGridTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
